c16e0e7b18122ca94cf7ccc91189c997b1704bd2
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2021-10-23 22:50:54 -0600

    bookings journeys done without frontend

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   # resources :users
-  resources :bookings, only: [:index]
+  resources :bookings, only: %i[index show]
   resources :services do
     collection do
       get :genre
