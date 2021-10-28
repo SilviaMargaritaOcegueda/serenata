bae7d8874c7b588356a048e8d203e5769819ffdf
Silvia Ocegueda <margarita.ocegueda@gmail.com>
2021-10-28 02:56:34 -0600

    seed file, placeholder for avatars

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_051821) do
+ActiveRecord::Schema.define(version: 2021_10_28_061323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2021_10_26_051821) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "demo_link"
     t.index ["user_id"], name: "index_services_on_user_id"
   end
 
